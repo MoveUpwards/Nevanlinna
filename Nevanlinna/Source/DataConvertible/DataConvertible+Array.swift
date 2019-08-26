f2b36aa8e6b4c065d9238accd938cd550cdeb3cd
Loïc GRIFFIE <lgriffie@mbp-de-loic.lan>
2019-08-26 16:39:53 +0200

    Add missing extension on DataConvertible for Array

---
+++ Nevanlinna/Source/DataConvertible/DataConvertible+Array.swift
@@ -0,0 +1,19 @@
+//
+//  DataConvertible+Array.swift
+//  Nevanlinna
+//
+//  Created by Loïc GRIFFIE on 26/08/2019.
+//  Copyright © 2019 Loïc GRIFFIE. All rights reserved.
+//
+
+import Foundation
+
+extension Array: DataConvertible where Element: UnsignedInteger {
+    public func toData() -> Data {
+        guard self is [UInt8] else {
+            return Data()
+        }
+
+        return Data(compactMap({ UInt8($0) }))
+    }
+}
