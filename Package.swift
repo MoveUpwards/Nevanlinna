36740b5665393b8f2e3cab42ed78f8dbf85a32e3
Loïc GRIFFIE <lgriffie@mbp-de-loic.lan>
2019-10-04 15:38:10 +0200

    Add SPM

--- Package.swift
+++ Package.swift
@@ -1,9 +1,12 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
     name: "Nevanlinna",
+    platforms: [
+        .iOS(.v9),
+    ],
     products: [
         .library(
             name: "Nevanlinna",
